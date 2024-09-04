5ce74aa83523d3c31a2837f019f2b3eaa63418d3
UserNameBlank <h.kowi2000@gmail.com>
2024-09-04 17:25:08 +0200

    [fix] android's back button

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,10 +2,19 @@ import { getLocaleFromNavigator, locale, waitLocale } from 'svelte-i18n';
 import '$lib/i18n';
 import type { LayoutLoad } from './$types';
 import { Preferences } from '@capacitor/preferences';
+import { App } from '@capacitor/app';
 
 export const prerender = true;
 export const ssr = false;
 export const load: LayoutLoad = async () => {
+	App.addListener('backButton', ({ canGoBack }) => {
+		if (!canGoBack) {
+			App.exitApp();
+		} else {
+			window.history.back();
+		}
+	});
+
 	locale.set((await Preferences.get({ key: 'language' })).value ?? getLocaleFromNavigator());
 	await waitLocale();
 };
